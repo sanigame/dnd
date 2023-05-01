3c1845b6b761676a3e68a5ef9f8e80e25ec37f07
Supasan Suklim <supasan.game@gmail.com>
2023-05-01 14:52:46 +0700

    Update toolbar

--- src/features/noteDetail/NoteDetail.js
+++ src/features/noteDetail/NoteDetail.js
@@ -6,11 +6,39 @@ import 'react-quill/dist/quill.snow.css'
 
 const NoteDetail = ({ noteId }) => {
   const [value, setValue] = useState('')
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+      ['link', 'image'],
+    ],
+  }
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+  ]
 
   return (
     <div>
       <p>NoteDetail {noteId}</p>
-      <ReactQuill theme="snow" value={value} onChange={setValue} />
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        value={value}
+        onChange={setValue}
+      />
     </div>
   )
 }
