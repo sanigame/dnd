516ec11e146e2c0360b601e7318760563f2afe7e
Supasan Suklim <supasan.game@gmail.com>
2023-05-01 14:47:33 +0700

    Add react-quill

--- src/features/noteDetail/NoteDetail.js
+++ src/features/noteDetail/NoteDetail.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PropTypes from 'prop-types'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 const NoteDetail = ({ noteId }) => {
-  return <div>NoteDetail {noteId}</div>
+  const [value, setValue] = useState('')
+
+  return (
+    <div>
+      <p>NoteDetail {noteId}</p>
+      <ReactQuill theme="snow" value={value} onChange={setValue} />
+    </div>
+  )
 }
 
 NoteDetail.propTypes = {
-  noteId: PropTypes.number,
+  noteId: PropTypes.string,
 }
 
 export default NoteDetail
