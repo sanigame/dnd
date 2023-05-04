9f877def8dd25ec44534ef1cd99473513cfd222e
Supasan Suklim <supasan.game@gmail.com>
2023-05-04 15:05:38 +0700

    Add first line large

--- src/features/noteDetail/NoteDetail.js
+++ src/features/noteDetail/NoteDetail.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
+import './style.css'
 
 const NoteDetail = ({ noteId }) => {
   const [value, setValue] = useState('')
   const modules = {
     toolbar: [
-      [{ header: [1, 2, false] }],
+      // [{ header: [1, 2, false] }],
       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
       [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
       ['link', 'image'],
